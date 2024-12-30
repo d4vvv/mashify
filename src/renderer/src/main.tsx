5b987139bbc03963948e58d3cecb20479bc5c8e0
d4vvv <kacper.godula99@gmail.com>
2024-12-31 00:38:39 +0100

    remove children from layout component

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './assets/index.css'
 import Layout from './components/Layout/Layout'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Layout>
-      <App />
-    </Layout>
+    <Layout />
   </React.StrictMode>
 )
